e6fd367fbc2c7695687b8fe6dbd1e868220d8dd9
MRtim <timatosalikaze@gmail.com>
2025-03-18 21:16:55 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Файловый менеджер");
+        Console.WriteLine("Выберите действие");
+        Console.WriteLine("1 - копировать");
+        Console.WriteLine("2 - удалить");
+        Console.WriteLine("3 - получить информацию");
+        Console.WriteLine("4 - выйти");
+
+        string Num = Console.ReadLine()!;
+        switch (Num)
+        {
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+                break;
+            default:
+                {
+                    Console.WriteLine("Ошибка");
+                    break;
+                }
+        }
+    }
+}
