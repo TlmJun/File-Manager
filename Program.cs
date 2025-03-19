89271bc946010343df8d648c2e3321787a17b81e
MRtim <timatosalikaze@gmail.com>
2025-03-19 18:20:19 +0500

    сделал макет

--- Program.cs
+++ Program.cs
@@ -5,11 +5,17 @@ class Program
 {
     static void Main(string[] args)
     {
+        FileManager manager  = new FileManager();
+        manager.ListFiles();
+        manager.ListFiles();
+        manager.ChangeDirectory("folder1");
+        manager.ListFiles();
+
         Console.WriteLine("Файловый менеджер");
         Console.WriteLine("Выберите действие");
         Console.WriteLine("1 - копировать");
         Console.WriteLine("2 - удалить");
-        Console.WriteLine("3 - получить информацию");
+        Console.WriteLine("3 - свойства");
         Console.WriteLine("4 - выйти");
 
         string Num = Console.ReadLine()!;
@@ -27,4 +33,39 @@ class Program
                 }
         }
     }
+
+    class FileManager
+    {
+        public string CurrentDirectory;
+        public FileManager()
+        {
+            CurrentDirectory = new Directory.GetCurrentDirectory();
+        }
+        public void ListFiles()
+        {
+            Console.WriteLine("Contents of" + CurrentDirectory);
+            foreach (var file in Directory.GetFiles(CurrentDirectory))
+            {
+                Console.WriteLine(Path.GetFileName(file));
+            }
+            Console.WriteLine("Contents of" + CurrentDirectory);
+            foreach (var dir in Directory.GetFiles(CurrentDirectory))
+            {
+                Console.WriteLine(Path.GetFileName(dir));
+            }
+        }
+        public void ChangeDirectory(string newDirectory)
+        {
+            string path = Path.Combine(CurrentDirectory, newDirectory);
+            if (Directory.Exists(path))
+            {
+                CurrentDirectory = path;
+                Console.WriteLine("Changed directory to" + CurrentDirectory);
+            }
+            else
+            {
+                Console.WriteLine("Directory not found:" + newDirectory);
+            }
+        }
+    } 
 }
